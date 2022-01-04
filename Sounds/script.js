c60fb17656e09db0c471e20834d7a3b0cdb00dbd
SaraKim12 <88058376+SaraKim12@users.noreply.github.com>
2022-01-04 15:51:23 -0300

    Add files via upload

---
+++ Sounds/script.js
@@ -0,0 +1,62 @@
+function Cliqueboi(){
+  var audio = new Audio()
+  audio.src = 'som_de_boi.mp3'
+  audio.loop = false
+  audio.play()
+}
+
+function Cliquedog(){
+    var audio = new Audio()
+    audio.src = 'cachorro_latindo.mp3'
+    audio.loop = false
+    audio.play()
+}
+
+function Cliquetigre(){
+  var audio = new Audio()
+  audio.src = 'tigre.mp3'
+  audio.loop = false
+  audio.play()
+}
+
+function Cliquegato(){
+  var audio = new Audio()
+  audio.src = 'som_de_gato.mp3'
+  audio.loop = false
+  audio.play()
+}
+
+function Cliqueaguia(){
+  var audio = new Audio()
+  audio.src = 'som_de_águia.mp3'
+  audio.loop = false
+  audio.play()
+}
+
+function Cliquecalopcita(){
+  var audio = new Audio()
+  audio.src = 'canto_calopsita.mp3'
+  audio.loop = false
+  audio.play()
+}
+
+function Cliquebode(){
+  var audio = new Audio()
+  audio.src = 'som_de_bodi.mp3'
+  audio.loop = false
+  audio.play()
+}
+
+function Cliquecobra(){
+  var audio = new Audio()
+  audio.src = 'som_de_cobra.mp3'
+  audio.loop = false
+  audio.play()
+}
+
+function Cliqueelefante(){
+  var audio = new Audio()
+  audio.src = 'som_de_elefante.mp3'
+  audio.loop = false
+  audio.play()
+}
